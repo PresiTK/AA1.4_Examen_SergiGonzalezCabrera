1c66b97f60a8e2ee32d94a1602a73c0b6d80ec02
PresiTK <sergi.gonzalez.cabrera@estudiant.enti.cat>
2025-11-07 17:42:11 +0100

    Movimiento Enemy

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,22 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 using static UnityEngine.GraphicsBuffer;
 
 public class EnemyController : MonoBehaviour
 {
     public float speedRotation;
     public float stoppingDistance;
+    public float speed = 2f;
+    private Rigidbody rb;
+    private Vector3 velocityChange;
+
 
     [SerializeField] private GameObject target;
     private Vector3 targetPos;
     private void Start()
     {
+        rb= GetComponent<Rigidbody>();  
         target = GameObject.FindGameObjectWithTag("Player");
     }
     private void FixedUpdate()
     {
         LookAtObject();
+        Vector3 direction = (transform.forward * 1 + transform.right * 0).normalized;
+        Vector3 firstVelocity = direction * speed;
+        Vector3 finalVelocity = rb.linearVelocity;
+        velocityChange = firstVelocity - new Vector3(finalVelocity.x, 0f, finalVelocity.z);
+        rb.AddForce(velocityChange, ForceMode.VelocityChange);
+        if((target.transform.position.x-transform.position.x<3) && (target.transform.position.x - transform.position.x > -3)&& (target.transform.position.z - transform.position.z < 3) && (target.transform.position.z - transform.position.z > -3))
+        {
+            speed = 1f;
+        }
+        else
+        {
+            speed = 2f;
+        }
+            Debug.Log(target.transform.position.x - transform.position.x);
+
     }
     private void Update()
     {
